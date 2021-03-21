252438711e758afafbd897afa38c4136a7f4f208
Jack Pye <jack.pye@codification.io>
2021-03-21 21:38:40 +0000

    simple go server

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello World!")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// log.Println("Hello World!")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+		// log.Printf("Data %s\n", d)
+		fmt.Fprintf(rw, "Hello %s\ns", d)
 	})
 
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World!")
+	})
 	http.ListenAndServe(":9090", nil)
 
 }
